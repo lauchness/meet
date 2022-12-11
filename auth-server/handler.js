2f06daca44a7c867d4c3f7bdd13550442a030daf
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-12-11 12:02:06 -0400

    trying

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -61,6 +61,9 @@ module.exports.getAccessToken = async (event) => {
       //respond with OAuth token
       return {
         statusCode: 200,
+        addHeaders: {
+          "Access-Control-Allow-Origin": "*",
+        },
         body: JSON.stringify(token),
       };
     })
