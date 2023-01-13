6370ba86e0958417906070746428468cadcedaae
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-01-13 11:52:52 -0400

    struggling to get puppeteer to work

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,13 +3,9 @@ import puppeteer from "puppeteer";
 describe("show/hide an event details", () => {
   let browser;
   let page;
-
   beforeAll(async () => {
     jest.setTimeout(30000);
-    browser = await puppeteer.launch({
-      //   headless: true,
-      //   slowMo: 50,
-    });
+    browser = await puppeteer.launch();
     page = await browser.newPage();
     await page.goto("http://localhost:3000/");
     await page.waitForSelector(".event");
@@ -20,19 +16,13 @@ describe("show/hide an event details", () => {
   });
 
   test("An event element is collapsed by default", async () => {
-    const eventDetails = await page.$(".event .event-details");
+    const eventDetails = await page.$(".event .event__Details");
     expect(eventDetails).toBeNull();
   });
 
   test("User can expand an event to see its details", async () => {
-    await page.click(".event .show-details");
-    const eventDetails = await page.$(".event .event-details");
+    await page.click(".event .details-btn");
+    const eventDetails = await page.$(".event .event__Details");
     expect(eventDetails).toBeDefined();
-  });
-
-  test("User can collapse an event to hide its details", async () => {
-    await page.click(".event .hide-details");
-    const eventDetails = await page.$(".event .event-details");
-    expect(eventDetails).toBeNull();
   });
 });
